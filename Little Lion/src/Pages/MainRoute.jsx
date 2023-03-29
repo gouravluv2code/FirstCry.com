3cef187b87aaf94aaabe80df01a8951ff6ce0467
Shubhainder <shubhainder7@hotmail.com>
2023-03-30 00:21:50 +0530

    login

--- Little Lion/src/Pages/MainRoute.jsx
+++ Little Lion/src/Pages/MainRoute.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../HomePage/Home";
-
+import Login from "../Login/Login";
 const MainRoute = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </div>
   );
