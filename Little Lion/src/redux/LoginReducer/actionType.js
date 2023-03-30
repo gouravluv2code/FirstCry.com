1d998fc155fba066a088a49038ae1743bd2173c1
Shubhainder <shubhainder7@hotmail.com>
2023-03-31 02:40:21 +0530

    redux added

---
+++ Little Lion/src/redux/LoginReducer/actionType.js
@@ -0,0 +1,2 @@
+export const SET_AUTH_TOKEN="SET_AUTH_TOKEN";
+ 
--- Little Lion/src/redux/store.js
+++ Little Lion/src/redux/store.js
@@ -1,13 +1,15 @@
 import {legacy_createStore,combineReducers,applyMiddleware} from "redux"
 
 import {reducer as adminReducer} from "./AdminReducer/reducer"
-import {reducer as cartReducer} from "./CartReducer/reducer"
+import {reducer as cartReducer} from "./CartReducer/reducer";
+import {reducer as loginReducer} from "./LoginReducer/reducer";
 
 
 import thunk from "redux-thunk"
 const rootReducer = combineReducers({
     adminReducer,
-    cartReducer
+    cartReducer,
+    loginReducer
 })
     
 
