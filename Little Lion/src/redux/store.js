efbce9656cde35bdc443d8b413d0e59c9c9674a2
gourav <gt9675tiwari@gmail.com>
2023-03-31 23:49:25 +0530

    fw22_0057 Day-4

--- Little Lion/src/redux/store.js
+++ Little Lion/src/redux/store.js
@@ -1,8 +1,6 @@
 import {legacy_createStore,combineReducers,applyMiddleware} from "redux"
 
 import {reducer as adminReducer} from "./AdminReducer/reducer"
- 
-import {reducer as cartReducer} from "./CartReducer/reducer";
 import {reducer as loginReducer} from "./LoginReducer/reducer";
  
 
@@ -10,8 +8,7 @@ import {reducer as loginReducer} from "./LoginReducer/reducer";
 import thunk from "redux-thunk"
 const rootReducer = combineReducers({
     adminReducer,
- 
-    cartReducer,
+
     loginReducer
  
 })
