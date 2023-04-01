18e8daddb1e4554eb735be9a6854ccb9b3b78181
ketan1227 <ketanbhatia27@gmail.com>
2023-04-01 22:07:11 +0530

    kkk

--- Little Lion/src/redux/AdminReducer/reducer.js
+++ Little Lion/src/redux/AdminReducer/reducer.js
@@ -1,9 +1,9 @@
-import { Get_Product_Success, Patch_product_success, Product_Failure, Product_Req, Product_Success,DELETE_ITEM } from "./actionTypes"
+import { Get_Product_Success, Patch_product_success, Product_Failure, Product_Req, Product_Success, Delete_product_success } from "./actionTypes"
 
 const initialState={
     isLoading:false,
     isError:false,
-   products:[]
+    products:[]
 }
 
 
@@ -11,24 +11,24 @@ export const reducer =(state=initialState,{type,payload})=>{
     switch(type){
         case Product_Req:
             return {...state,isLoading:true}
-            case Product_Success:{
-                return {...state,isLoading:false}
-            }
-            case Product_Failure:{
-                return{...state,isLoading:false,isError:true}
-            }
-            case Get_Product_Success:{
-                return {...state,isLoading:false,products:payload}
-            }
+        case Product_Success:{
+            return {...state,isLoading:false}
+        }
+        case Product_Failure:{
+            return{...state,isLoading:false,isError:true}
+        }
+        case Get_Product_Success:{
+            return {...state,isLoading:false,products:payload}
+        }
         case Patch_product_success:{
             return {...state,isLoading:false,}
         }
-        
-            default:{
-                return {...state}
-            }
+        case Delete_product_success:{
+            const updatedProducts = state.products.filter(product => product.id !== payload)
+            return {...state, isLoading:false, products: updatedProducts}
+        }
+        default:{
+            return {...state}
+        }
     }
-return <>
-</>
-
 }
